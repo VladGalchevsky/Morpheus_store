cb312ab58a9a84d4eb974803ccf256c54c6085f9
Vladyslav Halchevskyi <vladgalchevsky97@gmail.com>
2024-09-17 15:44:56 +0300

    depend

--- api/handlers/user_handler.py
+++ api/handlers/user_handler.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 from uuid import UUID
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import IntegrityError
@@ -15,10 +16,11 @@ logger = getLogger(__name__)
 
 user_router = APIRouter()
 
+async def get_user_dal(db_session: Annotated[AsyncSession, Depends(get_db)]) -> UserDAL:
+    return UserDAL(db_session=db_session)
 
-async def _create_new_user(body: UserCreate, session: AsyncSession) -> ShowUser:
-    async with session.begin():
-        user_dal = UserDAL(session)
+
+async def _create_new_user(body: UserCreate, user_dal: UserDAL) -> ShowUser:
         user = await user_dal.create_user(
             name=body.name,
             surname=body.surname,
@@ -33,74 +35,72 @@ async def _create_new_user(body: UserCreate, session: AsyncSession) -> ShowUser:
             is_active=user.is_active,
         )
         
-async def _delete_user(user_id, session: AsyncSession) -> UUID | None:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        deleted_user_id = await user_dal.delete_user(
-            user_id=user_id,
-        )
+async def _delete_user(user_id, user_dal: UserDAL) -> UUID | None:
+        deleted_user_id = await user_dal.delete_user(user_id=user_id)
         return deleted_user_id
 
 async def _update_user(updated_user_params: dict, 
                        user_id: UUID, 
-                       session: AsyncSession) -> UUID | None:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        updated_user_id = await user_dal.update_user(
-            user_id=user_id, **updated_user_params
-        )
+                       user_dal: UserDAL) -> UUID | None:
+        updated_user_id = await user_dal.update_user(user_id=user_id, 
+                                                     **updated_user_params)
         return updated_user_id
 
-async def _get_user_by_id(user_id, session: AsyncSession) -> UserWithOrderSummary | None:
-    async with session.begin():
-        user_dal = UserDAL(session)
+async def _get_user_by_id(user_id: UUID, 
+                          user_dal: UserDAL
+                          ) -> UserWithOrderSummary | None:
         user_with_orders = await user_dal.get_user_by_id_with_orders(user_id=user_id)
         return user_with_orders
 
 @user_router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
-async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> ShowUser:
+async def create_user(body: UserCreate, 
+                      user_dal: Annotated[UserDAL, Depends(get_user_dal)]) -> ShowUser:
     try:
-        return await _create_new_user(body, db)
+        return await _create_new_user(body, user_dal)
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}")
 
 @user_router.delete("/", response_model=DeleteUserResponse)
-async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> DeleteUserResponse:
-    deleted_user_id = await _delete_user(user_id, db)
+async def delete_user(user_id: UUID, 
+                      user_dal: Annotated[UserDAL, Depends(get_user_dal)]) -> DeleteUserResponse:
+    deleted_user_id = await _delete_user(user_id, user_dal)
     if deleted_user_id is None:
-        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
+        raise HTTPException(status_code=404, 
+                            detail=f"User with id {user_id} not found.")
     return DeleteUserResponse(deleted_user_id=deleted_user_id)
 
 @user_router.get("/", response_model=ShowUser)
-async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> ShowUser:
-    user = await _get_user_by_id(user_id, db)
+async def get_user_by_id(user_id: UUID, 
+                         user_dal: Annotated[UserDAL, Depends(get_user_dal)]) -> ShowUser:
+    user = await _get_user_by_id(user_id, user_dal)
     if user is None:
-        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
+        raise HTTPException(status_code=404, 
+                            detail=f"User with id {user_id} not found.")
     return user
 
 @user_router.patch("/", response_model=UpdatedUserResponse)
-async def update_user_by_id(
-    user_id: UUID, body: UpdateUserRequest, db: AsyncSession = Depends(get_db)
-) -> UpdatedUserResponse:
+async def update_user_by_id(user_id: UUID, 
+                            body: UpdateUserRequest, 
+                            user_dal: Annotated[UserDAL, Depends(get_user_dal)]
+                            ) -> UpdatedUserResponse:
     updated_user_params = body.dict(exclude_none=True)
     if updated_user_params == {}:
         raise HTTPException(
             status_code=422, 
             detail="At least one parameter for user update info should be provided"
             )
-    user = await _get_user_by_id(user_id, db)
+    user = await _get_user_by_id(user_id, user_dal)
     if user is None:
-        raise HTTPException(
-            status_code=404, 
-            detail=f"User with id {user_id} not found."
-            )
+        raise HTTPException(status_code=404, 
+                            detail=f"User with id {user_id} not found.")
     try:
         updated_user_id = await _update_user(
-            updated_user_params=updated_user_params, db=db, user_id=user_id
+            updated_user_params=updated_user_params, 
+            user_dal=user_dal, 
+            user_id=user_id
         )
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}")
-    updated_user_id = await _update_user(updated_user_params=updated_user_params, db=db, user_id=user_id)
     return UpdatedUserResponse(updated_user_id=updated_user_id)
